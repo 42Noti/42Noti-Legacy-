4fe7e4e437c4444a07d01d5af6885b912daf7b72
luckylooky2 <luckylooky2@naver.com>
2023-02-07 13:42:23 +0900

    design : fix global styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        div {
+          display: block;
+        }
+      `}</style>
+    </>
+  );
 }
