b0e658d76922f616843272edf7c4426a6ed84f1f
luckylooky2 <luckylooky2@naver.com>
2023-02-07 13:43:54 +0900

    design : mypage component css WIP

--- components/mypage/Profile.js
+++ components/mypage/Profile.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-// import Image from "next/image";
 import { Button } from "@mui/material";
-import styles from "../../src/styles/mypage/mypage.module.css";
+// import styles from "../../src/styles/mypage/mypage.module.css";
 
 const Profile = () => {
   const [myInfo, setMyInfo] = useState({
@@ -15,32 +14,80 @@ const Profile = () => {
 
   return (
     <>
-      <div>
-        <img src={myInfo.photoURL} width="100" height="100" alt="intra_photo" />
-      </div>
-      <div className="profile-description">
-        <h2 className="profile-name">{myInfo.intraID}</h2>
-        <p className="profile-email">{myInfo.email}</p>
-        <p className="profile-blackholes">{myInfo.blackholes}</p>
-        <div style={{ paddingTop: "10px" }}>
-          <Button
-            style={{
-              backgroundColor: "cornflowers",
-              fontFamily: "ubuntu-regular",
-            }}
-            variant="contained"
-            onClick={() =>
-              window.open(
-                `https://profile.intra.42.fr/users/chanhyle`,
-                "_blank"
-              )
-            }
-            type="button"
-          >
-            Intra profile
-          </Button>
+      <div className="mypage-profile">
+        <div className="body">
+          <div className="row">
+            <div className="profile-image">
+              <img
+                src={myInfo.photoURL}
+                width="100"
+                height="100"
+                alt="intra_photo"
+              />
+            </div>
+            <div className="profile-description">
+              <h2 className="profile-name">{myInfo.intraID}</h2>
+              <p className="profile-email">{myInfo.email}</p>
+              <p className="profile-blackholes">{myInfo.blackholes}</p>
+              <div style={{ paddingTop: "10px" }}>
+                <Button
+                  style={{
+                    backgroundColor: "cornflowers",
+                    fontFamily: "ubuntu-regular",
+                  }}
+                  variant="contained"
+                  onClick={() =>
+                    window.open(
+                      `https://profile.intra.42.fr/users/chanhyle`,
+                      "_blank"
+                    )
+                  }
+                  type="button"
+                >
+                  Intra profile
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <style jsx>{`
+        .mypage-profile {
+          display: flex;
+          justify-content: center;
+        }
+        h1 {
+          color: #ffb6c1;
+          text-shadow: 2px 2px 2px #dbdbdb;
+        }
+
+        .body {
+          font-size: 14px;
+          color: #424242;
+          padding: 20px;
+          font-weight: 400;
+          background-color: pink;
+        }
+
+        .row {
+          display: flex;
+        }
+
+        .profile-image {
+          padding-right: 20px;
+        }
+
+        .profile-image img {
+          border-radius: 50%;
+          // @include profile-image-size($pc-img-size);
+          border: 0px solid #fff;
+          box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16),
+            0 3px 6px rgba(0, 0, 0, 0.23);
+        }
+
+        .profile-description {
+        }
+      `}</style>
     </>
   );
 };
--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -5,13 +5,44 @@ import styles from "../../src/styles/mypage/mypage.module.css";
 
 const MyPage = () => {
   return (
-    <div>
-      <h1>User Info.</h1>
-      <div>
+    <>
+      <div className="mypage-container">
+        <h1>User Info.</h1>
         <Profile />
         <AttendanceInfo />
       </div>
-    </div>
+      <style jsx global>{`
+        .mypage-container {
+          display: flex;
+          flex-direction: column;
+          margin: auto;
+          width: 80%;
+        }
+
+        h1 {
+          color: #ffb6c1;
+          text-shadow: 2px 2px 2px #dbdbdb;
+        }
+
+        card {
+          margin-left: 10px;
+          margin-right: 10px;
+          background-color: #fef8fb;
+          margin-bottom: 30px;
+          transition: 0.5s;
+          border: 0;
+          border-radius: 20px;
+          display: inline-block;
+          position: relative;
+          width: 100%;
+          box-shadow: none;
+        }
+
+        profile-header {
+          position: relative;
+        }
+      `}</style>
+    </>
   );
 };
 
