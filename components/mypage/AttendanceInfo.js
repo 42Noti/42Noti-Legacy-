1b335feb4c8bd08f003117154674e30571a1a07d
luckylooky2 <luckylooky2@naver.com>
2023-02-08 19:46:02 +0900

    fix : remove unused components

--- components/mypage/AttendanceInfo.js
+++ components/mypage/AttendanceInfo.js
@@ -1,13 +1,5 @@
-import React, { useState } from "react";
-
 const AttendanceInfo = () => {
-  const [isWallet, setIsWallet] = useState(false);
-  return (
-    <>
-      <h2>3개월 출석 정보</h2>
-      <h2>월렛 획득 여부 : {isWallet ? "👍" : "👎"}</h2>
-    </>
-  );
+  return;
 };
 
 export default AttendanceInfo;
--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -1,46 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../../components/mypage/Profile";
 import AttendanceInfo from "../../components/mypage/AttendanceInfo";
-import styles from "../../src/styles/mypage/mypage.module.css";
+import { Divider } from "@mui/material";
 
 const MyPage = () => {
+  const [isWallet, setIsWallet] = useState(false);
   return (
     <>
-      <div className="mypage-container">
-        <h1>User Info.</h1>
-        <Profile />
-        <AttendanceInfo />
+      <div className="global-container">
+        <div className="mypage-container">
+          <h1>User Info.</h1>
+          <Divider />
+          <Profile />
+          <h1>Attendance Calendar</h1>
+          <Divider />
+          <h1>Wallet? {isWallet ? "👍" : "👎"}</h1>
+        </div>
       </div>
-      <style jsx global>{`
+      <style jsx>{`
         .mypage-container {
           display: flex;
           flex-direction: column;
           margin: auto;
-          width: 80%;
+          width: 90%;
         }
 
         h1 {
-          color: #ffb6c1;
+          color: #000000;
           text-shadow: 2px 2px 2px #dbdbdb;
         }
 
-        card {
-          margin-left: 10px;
-          margin-right: 10px;
-          background-color: #fef8fb;
-          margin-bottom: 30px;
-          transition: 0.5s;
-          border: 0;
-          border-radius: 20px;
-          display: inline-block;
-          position: relative;
-          width: 100%;
-          box-shadow: none;
-        }
-
-        profile-header {
-          position: relative;
-        }
+        // .card {
+        //   margin-left: 10px;
+        //   margin-right: 10px;
+        //   background-color: #fef8fb;
+        //   margin-bottom: 30px;
+        //   transition: 0.5s;
+        //   border: 0;
+        //   border-radius: 20px;
+        //   display: inline-block;
+        //   position: relative;
+        //   width: 100%;
+        //   box-shadow: none;
+        // }
       `}</style>
     </>
   );
