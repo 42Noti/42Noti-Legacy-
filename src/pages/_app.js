df175a2dd96c07670c8f6bec13cb0410cc6493c1
luckylooky2 <luckylooky2@naver.com>
2023-02-08 19:41:26 +0900

    design : remove global classname

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,29 +7,23 @@ export default function App({ Component, pageProps }) {
       <NavBar />
       <Component {...pageProps} />
       <style jsx global>{`
-        .global-container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100%;
-          width: 100%;
-        }
-
         // pc : 768px ~
-        .global {
-          width: 750px;
-          height: 900px;
-          font-size: 16px;
+        .global-container {
+          width: 40%;
+          min-width: 650px;
+          height: 90%;
+          min-height: 700px;
+          font-size: 12px;
           background: #eeeeee;
           border-radius: 20px;
           // border: solid pink 1px;
           // box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
-          margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
+          margin: 30px auto; /* 페이지 중앙에 나타나도록 설정 */
           display: flex;
           flex-direction: column;
         }
 
-        // // tablet : 480px ~ 768px
+        // tablet : 480px ~ 768px
         // @media screen and (min-width: 480px) and (min-height: 600px) {
         //   .global {
         //     // width: 450px;
@@ -38,7 +32,7 @@ export default function App({ Component, pageProps }) {
         //   }
         // }
 
-        // // mobile : 320px ~ 480px
+        // mobile : 320px ~ 480px
         // @media screen and (min-width: 320px) and (min-height: 400px) {
         //   .global {
         //     // width: 280px;
