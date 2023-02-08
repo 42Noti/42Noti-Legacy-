456d4e0e421a1c5cf4df63b87d743e7fe02d783d
DoH <doh@DoHui-MacBookPro.local>
2023-02-08 18:00:57 +0900

    chore : components 폴더 위치 이동

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import { TodoProvider } from "components/TodoList/src/TodoContext";
-import TodoTemplate from "components/TodoList/TodoTemplate";
-import TodoHead from "components/TodoList/TodoHead";
-import TodoList from "components/TodoList/TodoList";
-import TodoCreate from "components/TodoList/TodoCreate";
-import Calendar from "components/TodoList/Calendar";
+import { TodoProvider } from "../components/TodoList/src/TodoContext";
+import TodoTemplate from "../components/TodoList/TodoTemplate";
+import TodoHead from "../components/TodoList/TodoHead";
+import TodoList from "../components/TodoList/TodoList";
+import TodoCreate from "../components/TodoList/TodoCreate";
+import Calendar from "../components/TodoList/Calendar";
+import TodoCategory from "../components/TodoList/TodoCategory";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <TodoTemplate>
         <TodoHead />
         <Calendar />
+        <TodoCategory />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
