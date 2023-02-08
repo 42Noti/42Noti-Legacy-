c0c1b542c4aa279b188cfd7b39b5569967960e54
luckylooky2 <luckylooky2@naver.com>
2023-02-08 15:37:12 +0900

    feat : navigation bar, global width/height

---
+++ components/common/NavBar.js
@@ -0,0 +1,136 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Link from "next/link";
+import { styled } from "@mui/material/styles";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export default function NavBar() {
+  const [sidebar, setSidebar] = React.useState(false);
+
+  const MyPageButton = () => {
+    return (
+      <Link href="/mypage" style={{ textDecoration: "none" }}>
+        <IconButton>
+          <AccountCircleIcon fontSize="large" />
+        </IconButton>
+      </Link>
+    );
+  };
+
+  const SideBarButton = () => {
+    return (
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={toggleDrawer(true)}
+        sx={{ mr: 2 }}
+      >
+        <MenuIcon />
+      </IconButton>
+    );
+  };
+
+  const MainLogo = () => {
+    return (
+      <Typography
+        variant="h6"
+        component="h1"
+        color="textSecondary"
+        align="center"
+        sx={{ flexGrow: 1 }}
+      >
+        <Link href="/" style={{ textDecoration: "none" }}>
+          42Manager
+        </Link>
+      </Typography>
+    );
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["로그인", "To-do", "마이페이지"].map((text, index) => (
+          <Link
+            href="/"
+            style={{ color: "black", textDecoration: "none" }}
+            key={text}
+          >
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setSidebar(open);
+  };
+
+  return (
+    <React.Fragment key="left">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <SideBarButton />
+            <MainLogo />
+            <MyPageButton />
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer open={sidebar} onClose={toggleDrawer(false)}>
+        <DrawerHeader>
+          <IconButton onClick={toggleDrawer(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        {list()}
+      </Drawer>
+    </React.Fragment>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,51 @@
 import "@/styles/globals.css";
+import NavBar from "components/common/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <NavBar />
       <Component {...pageProps} />
       <style jsx global>{`
-        div {
-          display: block;
+        .global-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
+          width: 100%;
         }
+
+        // pc : 768px ~
+        .global {
+          width: 750px;
+          height: 900px;
+          font-size: 16px;
+          background: #eeeeee;
+          border-radius: 20px;
+          // border: solid pink 1px;
+          // box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+          margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
+          display: flex;
+          flex-direction: column;
+        }
+
+        // // tablet : 480px ~ 768px
+        // @media screen and (min-width: 480px) and (min-height: 600px) {
+        //   .global {
+        //     // width: 450px;
+        //     // height: 550px;
+        //     font-size: 14px;
+        //   }
+        // }
+
+        // // mobile : 320px ~ 480px
+        // @media screen and (min-width: 320px) and (min-height: 400px) {
+        //   .global {
+        //     // width: 280px;
+        //     // height: 350px;
+        //     font-size: 12px;
+        //   }
+        // }
       `}</style>
     </>
   );
