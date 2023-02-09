6920d7069538715cf3146b08b066c602c96819d6
luckylooky2 <luckylooky2@naver.com>
2023-02-09 11:58:39 +0900

    feat : custom navigation bar pagelist

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -7,8 +7,9 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import EventNoteIcon from "@mui/icons-material/EventNote";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Link from "next/link";
 import { styled } from "@mui/material/styles";
@@ -30,7 +31,7 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 }));
 
 export default function NavBar() {
-  const [sidebar, setSidebar] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   const MyPageButton = () => {
     return (
@@ -73,32 +74,70 @@ export default function NavBar() {
     );
   };
 
-  const list = () => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        {["홈", "로그인", "To-do", "마이페이지"].map((text, index) => (
-          <Link
-            href="/"
-            style={{ color: "black", textDecoration: "none" }}
-            key={text}
-          >
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    </Box>
+  const setLink = (text) => {
+    if (text === "홈") return "/";
+    else if (text === "To-Do") return "/";
+    else if (text === "마이페이지") return "/mypage";
+    else if (text === "42 Intra") return "https://profile.intra.42.fr/";
+    else if (text === "Gather")
+      return "https://app.gather.town/app/Zq3peLuvz5isVQ0f/42seoul";
+    else if (text === "로그아웃") return "/logout";
+  };
+
+  const setIcon = (text) => {
+    if (text === "홈") return <HomeIcon />;
+    else if (text === "To-Do") return <EventNoteIcon />;
+    else if (text === "마이페이지") return <PersonIcon />;
+    else if (text === "42 Intra") return "42.png";
+    else if (text === "Gather") return "gather.png";
+  };
+
+  const PageList = ({ list }) => (
+    <List>
+      {list.map((text) => (
+        <Link
+          href={setLink(text)}
+          style={{ color: "black", textDecoration: "none" }}
+          key={text}
+        >
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              {text === "로그아웃" ? (
+                ""
+              ) : (
+                <ListItemIcon>{setIcon(text)}</ListItemIcon>
+              )}
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      ))}
+    </List>
+  );
+
+  const CustomPageList = () => (
+    <List>
+      {["42 Intra", "Gather"].map((text, index) => (
+        <Link
+          href={setLink(text)}
+          target="_blank"
+          style={{ color: "black", textDecoration: "none" }}
+          key={text}
+        >
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <div style={{ minWidth: 56 }}>
+                <img
+                  src={setIcon(text)}
+                  style={{ width: 30, height: 30, marginTop: 7 }}
+                />
+              </div>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      ))}
+    </List>
   );
 
   const toggleDrawer = (open) => (event) => {
@@ -108,7 +147,7 @@ export default function NavBar() {
     ) {
       return;
     }
-    setSidebar(open);
+    setIsOpen(open);
   };
 
   return (
@@ -122,14 +161,25 @@ export default function NavBar() {
           </Toolbar>
         </AppBar>
       </Box>
-      <Drawer open={sidebar} onClose={toggleDrawer(false)}>
+      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
         <DrawerHeader>
           <IconButton onClick={toggleDrawer(false)}>
             <ChevronLeftIcon />
           </IconButton>
         </DrawerHeader>
         <Divider />
-        {list()}
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <PageList list={["홈", "To-Do", "마이페이지"]} />
+          <Divider />
+          <CustomPageList />
+          <Divider />
+          <PageList list={["로그아웃"]} />
+        </Box>
       </Drawer>
     </React.Fragment>
   );
