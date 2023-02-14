25bc762d1b2d63d4ac8233a72fc3018bc1488f03
luckylooky2 <luckylooky2@naver.com>
2023-02-14 14:15:06 +0900

    design : missing todolist height fixing

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,10 +4,8 @@ import TodoItem from "./TodoItem";
 import { useSelector, useDispatch } from "react-redux";
 
 const TodoListBlock = styled.div`
-  flex: 1;
-  padding: 20px 32px;
-  padding-bottom: 48px;
-  overflow-y: auto;
+  height: 207px;
+  overflow: auto;
 `;
 
 function TodoList() {
