25b183eba54754491418245a57b133b00d4abd54
luckylooky2 <luckylooky2@naver.com>
2023-02-14 14:11:51 +0900

    design : outer frame 700px fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,25 +7,24 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <div className="global-container">
-        <div>
-          <Provider store={store}>
-            <NavBar />
-            <Component {...pageProps} />
-           </Provider>
-        </div>
+        <Provider store={store}>
+          <NavBar />
+          <Component {...pageProps} />
+        </Provider>
       </div>
       <style jsx>{`
         // mobile : 320px ~ 480px
         .global-container {
           width: 100%;
           min-width: 320px;
-          height: 90%;
-          min-height: 700px;
+          // height : 90%는 추가하지 말 것!
+          // height는 13인치 노트북에 맞춰서 700px로 고정
+          height: 700px;
           font-size: 10px;
-          background: #eeeeee;
+          background: #ffffff;
           border-radius: 20px;
           // box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
-          margin: 30px auto; /* 페이지 중앙에 나타나도록 설정 */
+          margin: 20px auto; /* 페이지 중앙에 나타나도록 설정 */
           display: flex;
           flex-direction: column;
         }
