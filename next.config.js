7b8f1c98b21081086911fe43e658a7c3a3ac4d26
luckylooky2 <luckylooky2@naver.com>
2023-02-22 11:26:11 +0900

    fix : react strict mode off

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: '.',
-}
+  // reactStrictMode: true,
+  assetPrefix: ".",
+};
 
 module.exports = {
   nextConfig,
